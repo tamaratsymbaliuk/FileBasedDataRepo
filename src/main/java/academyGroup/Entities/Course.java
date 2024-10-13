c8bdd8cc00f9416a4fdf08e00937efc2e6124fdc
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-10-12 21:50:40 -0500

    File reader program - Course class

---
+++ src/main/java/academyGroup/Entities/Course.java
@@ -0,0 +1,37 @@
+package academyGroup.Entities;
+
+public class Course {
+    private int id;
+    private String name;
+    private int academyId;
+
+    public Course(int id, String name, int academyId) {
+        this.id = id;
+        this.name = name;
+        this.academyId = academyId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAcademyId() {
+        return academyId;
+    }
+
+    public void setAcademyId(int academyId) {
+        this.academyId = academyId;
+    }
+}

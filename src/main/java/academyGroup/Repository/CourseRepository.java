6e587078324708d995270fdffe876cec4cb5e4d3
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-10-13 22:42:48 -0500

    File reader program - CourseRepository class

---
+++ src/main/java/academyGroup/Repository/CourseRepository.java
@@ -0,0 +1,32 @@
+package academyGroup.Repository;
+
+import academyGroup.Entities.Course;
+
+import java.util.List;
+
+public class CourseRepository implements IRepository<Course> {
+    @Override
+    public List<Course> getAll() {
+        return null;
+    }
+
+    @Override
+    public Course getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void add(Course entity) {
+
+    }
+
+    @Override
+    public void update(Course entity) {
+
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+}

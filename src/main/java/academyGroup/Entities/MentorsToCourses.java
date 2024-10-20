604a3b7bd5092cea78d6d7cb9e380124f9fbccf6
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-10-20 17:37:40 -0500

    File reader program - Entities

--- src/main/java/academyGroup/Entities/MentorsToCourses.java
+++ src/main/java/academyGroup/Entities/MentorsToCourses.java
@@ -2,6 +2,10 @@ package academyGroup.Entities;
 
 import java.io.Serializable;
 
+/**
+ * The MentorsToCourses class represents a many-to-many relationship between mentors and courses.
+ * It includes the relationship's ID, course ID, mentor ID, and group ID.
+ */
 public class MentorsToCourses implements Serializable {
     // many-to-many relationship
     private int id;

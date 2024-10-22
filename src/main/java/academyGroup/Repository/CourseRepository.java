d115cda394baaf8ca0e33a5422f7def1edf46a2b
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-10-21 22:14:26 -0500

    File reader program - CourseRepository

--- src/main/java/academyGroup/Repository/CourseRepository.java
+++ src/main/java/academyGroup/Repository/CourseRepository.java
@@ -1,32 +1,91 @@
 package academyGroup.Repository;
 
 import academyGroup.Entities.Course;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Manages CRUD operations for Academy entities using a DbSet.
  */
 public class CourseRepository implements IRepository<Course> {
+    DbContext context;
+    public CourseRepository(DbContext context) {
+        this.context = context;
+    }
+
     @Override
-    public Map<Integer,Course> getAll() {
+    public Map<String,Course> getAll() {
+        DbSet dbSet = context.getDatabaseFromFile();
+        return new HashMap<String,Course>(dbSet.getCourses());
+    }
+
+    @Override
+    public Course getById(String id) {
+        Map<String,Course> courses = getAll();
+        if (courses.containsKey(id)) {
+            return courses.get(id);
+        }
+
         return null;
     }
 
     @Override
-    public Course getById(int id) {
-        return null;
+    public String add(Course course) {
+        DbSet dbSet = context.getDatabaseFromFile();
+
+        // add to Course table
+        String id = UUID.randomUUID().toString();
+        course.setId(id); // generate new UUID random string and set it for Id of an entity
+        Map<String,Course> courses = new HashMap<String,Course>(dbSet.getCourses());
+        courses.put(course.getId(), course);
+        dbSet.setCourses(courses);
+
+        // update academyId index
+        Set<String> indices = dbSet.getCoursesAcademyIndex(course.getAcademyId());
+        indices.add(id);
+        dbSet.setCoursesAcademyIndex(course.getAcademyId(), indices);
+
+        context.SaveChangesToFile(dbSet);
+
+        return id;
     }
 
     @Override
-    public void add(Course entity) {
+    public void update(Course course) {
+        DbSet dbSet = context.getDatabaseFromFile();
 
+        // Update Course table
+        Map<String,Course> courses = new HashMap<String,Course>(dbSet.getCourses());
+        courses.put(course.getId(), course);
+        dbSet.setCourses(courses);
+
+        // update academyId index
+        Set<String> indices = dbSet.getCoursesAcademyIndex(course.getAcademyId());
+        indices.add(course.getId());
+        dbSet.setCoursesAcademyIndex(course.getAcademyId(), indices);
+
+        context.SaveChangesToFile(dbSet);
     }
+
     @Override
-    public void update(Course entity) {
+    public void remove(String id) {
+        DbSet dbSet = context.getDatabaseFromFile();
 
-    }
-    @Override
-    public void remove(int id) {
+        // Remove from Course table
+        Map<String,Course> courses = new HashMap<String,Course>(dbSet.getCourses());
+        String academyId = courses.get(id).getAcademyId();
+        courses.remove(id);
+        dbSet.setCourses(courses);
 
+        // remove from index
+        Set<String> indices = dbSet.getCoursesAcademyIndex(academyId);
+        indices.remove(id);
+        dbSet.setCoursesAcademyIndex(academyId, indices);
+
+        context.SaveChangesToFile(dbSet);
     }
+
 }
